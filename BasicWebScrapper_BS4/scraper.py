8763d07cb5e330aee5a14b2984bcb396ae915bea
Charles Vanchieri <cvanchieri6@charless-mbp-2.lan>
2021-04-29 16:02:10 -0700

    save scraped data to a local csv file.

--- BasicWebScrapper_BS4/scraper.py
+++ BasicWebScrapper_BS4/scraper.py
@@ -5,6 +5,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import csv 
 
 ### html file ### 
 # with open('simple.html') as html_file:
@@ -31,10 +32,19 @@ print('-Title-')
 print(title)
 print('---')
 #print(article.prettify())
+
+csv_file = open('blog_scrape.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['author_image', 'author_name', 'header_text', 'body_text', 'content_link'])
+
 for post in soup.find_all('div', class_='blogpost vcard'):
     author_image = post.div.span.a.span.img['src']
     author_name = post.p.a.text
     header_text = post.h3.a.text
+    body = post.find('div', class_='xg_user_generated')
+    body_text = ''
+    content_link = ''
     
     print('#######-Blog Post-#######')
     print('-Content Author Image-')
@@ -51,38 +61,58 @@ for post in soup.find_all('div', class_='blogpost vcard'):
         print('None')
 
     print('-Content Body Text-')
-    body = post.find('div', class_='xg_user_generated')
     try:
         if body.select_one('.xg_user_generated li:nth-child(1)').text != '':
-            print(body.select_one('.xg_user_generated li:nth-child(1)').text)
+            body1 = body.select_one('.xg_user_generated li:nth-child(1)').text
+            body_text = body1
+            print(body1)
     except Exception as e:
-        pass  
+        body1 = '' 
+        body_text = body1
     try:
         if body.select_one('.xg_user_generated li:nth-child(2)').text != '':
-            print(body.select_one('.xg_user_generated li:nth-child(2)').text)
+            body2 = body.select_one('.xg_user_generated li:nth-child(2)').text
+            body_text = body1 + body2
+            print(body2)
     except Exception as e:
-        pass  
+        body2 = ''  
+        body_text = body1 + body2
     try:
         if body.select_one('.xg_user_generated p:nth-child(2)').text != '':
-            print(body.select_one('.xg_user_generated p:nth-child(2)').text)
+            body3 = body.select_one('.xg_user_generated p:nth-child(2)').text
+            body_text = body1 + body2 + body3
+            print(body3)
     except Exception as e:
-        pass   
+        body3 = ''
+        body_text = body1 + body2 + body3   
     try:
         if body.select_one('.xg_user_generated p:nth-child(3)').text != '':
-            print(body.select_one('.xg_user_generated p:nth-child(3)').text)
+            body4 = body.select_one('.xg_user_generated p:nth-child(3)').text
+            body_text = body1 + body2 + body3 + body4
+            print(body4)
     except Exception as e:
-        pass
+        body4 = ''
+        body_text = body1 + body2 + body3 + body4
     try:
         if body.select_one('.xg_user_generated p:nth-child(4)').text != '':
-            print(body.select_one('.xg_user_generated p:nth-child(4)').text)
+            body5 = body.select_one('.xg_user_generated p:nth-child(4)').text
+            body_text = body1 + body2 + body3 + body4 + body5
+            print(body5)
     except Exception as e:
-        pass
+        body5 = ''
+        body_text = body1 + body2 + body3 + body4 + body5
     print('-Contenet Link-')
     try:
         if body.find('a', class_=['xj_expandable'])['href'] is not None:
-            print(body.find('a', class_=['xj_expandable'])['href'])
+            content_link = body.find('a', class_=['xj_expandable'])['href']
+            print(content_link)
     except Exception as e:
-        pass
+        print('None')
 
+    if body_text == '':
+        body_text = ' None'
 
+    csv_writer.writerow([author_image, author_name, header_text, body_text, content_link])
+
+csv_file.close()
 
